2e09211ef56050d0be6e6cd42ca6a7c3f51bd610
RogerWillis <roger.willis@r3cev.com>
2017-05-12 10:04:39 -0400

    Minor tweaks.

--- src/main/kotlin/net/corda/yo/Client.kt
+++ src/main/kotlin/net/corda/yo/Client.kt
@@ -1,9 +1,9 @@
 package net.corda.yo
 
 import com.google.common.net.HostAndPort
+import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.loggerFor
-import net.corda.client.rpc.CordaRPCClient
 import org.slf4j.Logger
 import rx.Observable
 
@@ -15,8 +15,9 @@ private class YoRPC {
     companion object {
         val logger: Logger = loggerFor<YoRPC>()
     }
+
     fun main(args: Array<String>) {
-        require(args.size == 1) { "Usage: TemplateClientRPC <node address:port>" }
+        require(args.size == 1) { "Usage: YoRPC <node address:port>" }
         val nodeAddress = HostAndPort.fromString(args[0])
         val client = CordaRPCClient(nodeAddress)
         // Can be amended in the com.template.MainKt file.
@@ -25,7 +26,7 @@ private class YoRPC {
         // Grab all signed transactions and all future signed transactions.
         val (transactions: List<SignedTransaction>, futureTransactions: Observable<SignedTransaction>) =
                 proxy.verifiedTransactions()
-        // Log the existing TemplateStates and listen for new ones.
+        // Log the existing Yo's and listen for new ones.
         futureTransactions.startWith(transactions).toBlocking().subscribe { transaction ->
             transaction.tx.outputs.forEach { output ->
                 val state = output.data as Yo.State
